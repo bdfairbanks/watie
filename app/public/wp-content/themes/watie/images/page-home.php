a02e2245f093cea8a99546ed80b9274837bc7386
Ben Fairbanks <bdfairbanks3@gmail.com>
2017-04-05 13:31:45 -0500

    added home

--- app/public/wp-content/themes/watie/images/page-home.php
+++ app/public/wp-content/themes/watie/images/page-home.php
@@ -1,6 +1,7 @@
 <?php get_header(); ?>
 <main>
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+   <?php if ( the_category() == True ): echo "dummy"; ?>
    <?php the_title(); ?> 
      <p>Hello world fart</p>
    <?php the_content(); ?> 
